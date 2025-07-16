ad66a646733754a1b5c97969fa5e9454536de629
Iemtabish <109301389+Iemtabish@users.noreply.github.com>
2025-07-16 17:41:18 +0000

    learning

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountID = "144553"
+let accountEmail = "tabish@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 
+
+console.log(accountID);
+
+
+accountEmail = "ayesha@google.com"
+accountPassword = "21345"
+accountCity = "muzaffarpur"
+
+console.table([accountEmail, accountPassword, accountCity, accountID])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as newer version
+
+alert("hello")
+
+// number - 2,3,5.6,100.12
+// string -  "  "
+// bigint
+// boolean - true/false
+// null - standalone value
+// undefined
+// symbol - uniqueness
+// object - 
+
