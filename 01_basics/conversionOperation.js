a56a7629ef8bdbc13c4c798f55a8bcaabb86c822
Iemtabish <109301389+Iemtabish@users.noreply.github.com>
2025-07-16 18:20:17 +0000

    Conversion of Data Types

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -2,16 +2,16 @@ let score = "69"
 
 let valueInNumber = Number(score)
 
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
 
 
 let isLoggedIn = "Tabish"
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
 
-// console.log(typeof booleanisLoggedIn);
-// console.log(booleanisLoggedIn);
+console.log(typeof booleanisLoggedIn);
+console.log(booleanisLoggedIn);
 
 
 
