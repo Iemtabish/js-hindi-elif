799db65e70e033714f14a790d592ad670ee91b2d
Iemtabish <109301389+Iemtabish@users.noreply.github.com>
2025-07-17 08:25:54 +0000

    datatype-summary

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "69"
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = "Tabish"
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,9 @@
+// Data TYPES 
+
+// Primitive
+
+// 7 Types : String, Boolean, Number, undefined, Symbol, BigINT
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
