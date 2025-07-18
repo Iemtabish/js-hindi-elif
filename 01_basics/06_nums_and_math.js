3c9d377c060aa06d0c6b972e05b2f8c801db2315
Iemtabish <109301389+Iemtabish@users.noreply.github.com>
2025-07-18 18:31:57 +0000

    nums & math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.round(4.3));
+// console.log(Math.min(1,4,5,8,9));
+// console.log(Math.max(4,5,8,9));
+
+console.log(Math.random());
